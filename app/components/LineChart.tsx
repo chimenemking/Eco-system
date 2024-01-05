9b4ba51db2b0c0f4c5163e91388ac6bb37d28b4b
chimenem Okandu <chimenemwillis@gmail.com>
2024-01-05 02:52:51 +0100

    Fixed functional component issue by changing structure of customTooltip

---
+++ app/components/LineChart.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import React from 'react'
+import {LineChart, Line} from 'recharts'
+
+type Props = {}
+
+
+
+const LineChartComponent = (props: Props) => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default LineChartComponent
